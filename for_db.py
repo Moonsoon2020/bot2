01ff012d4aadc8451b083269d6c4e9ced44f46cc
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2023-03-17 20:48:56 +0300

    23

---
+++ for_db.py
@@ -0,0 +1,322 @@
+import sqlite3
+
+import xlsxwriter
+import pandas as pd
+
+
+def createBD():  # инициализация класса
+    con = sqlite3.connect('database.db', check_same_thread=False)  # подключение БД
+    create_table1 = """CREATE TABLE IF NOT EXISTS assortment (
+    id    INTEGER    PRIMARY KEY AUTOINCREMENT,
+    name        STRING  NOT NULL,
+    http        STRING,
+    description STRING,
+    category    INT     REFERENCES category (id) 
+);
+
+"""
+    create_table2 = """CREATE TABLE IF NOT EXISTS discounts (
+     id  INTEGER   PRIMARY KEY AUTOINCREMENT,
+    name   STRING  NOT NULL,
+    des_if STRING
+);
+
+
+"""
+    create_table3 = """CREATE TABLE IF NOT EXISTS notifications (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    text    TEXT,
+    time
+);
+"""
+    create_table4 = """CREATE TABLE IF NOT EXISTS question_answer (
+    key_words STRING NOT NULL,
+    answer           NOT NULL
+);
+"""
+    create_table5 = """CREATE TABLE IF NOT EXISTS users (
+     id     INTEGER PRIMARY KEY AUTOINCREMENT,
+    name   STRING  NOT NULL,
+    status BOOLEAN NOT NULL,
+    id_tg  INTEGER NOT NULL
+                   UNIQUE,
+    username STRING
+);
+"""
+    create_table6 = """CREATE TABLE IF NOT EXISTS category ( id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name STRING, 
+    http STRING
+);
+        """
+    con.cursor().execute(create_table1)  # создание отсутствующих и необходимых таблиц
+    con.cursor().execute(create_table2)
+    con.cursor().execute(create_table3)
+    con.cursor().execute(create_table4)
+    con.cursor().execute(create_table5)
+    con.cursor().execute(create_table6)
+    con.commit()
+
+
+def get_answer(key_words):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    return con.cursor().execute(f'''SELECT answer FROM question_answer WHERE {key_words.capitalize()}''').fetchone()
+
+
+def add_que_ans(question, answer):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''INSERT INTO question_answer(key_words, answer)
+             VALUES ({question}, {answer})''')
+    con.commit()
+
+
+def get_id_category_of_name(name):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    return con.cursor().execute(f'''SELECT id FROM category WHERE name="{name}"''').fetchone()[0]
+
+
+def get_category():
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    return con.cursor().execute(f'''SELECT * FROM category''').fetchall()
+
+
+def get_assort_name_category(name):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    id_category = get_id_category_of_name(name)
+    return con.cursor().execute(f'''SELECT id, name, description, http
+             FROM assortment WHERE category = "{id_category}"''').fetchall()
+
+
+def get_category_assort(id_category):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    return con.cursor().execute(f'''SELECT id, name, description, http
+          FROM assortment WHERE category = "{id_category}"''').fetchall()
+
+
+def add_category(name, http):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''INSERT INTO category(name, http)
+                         VALUES('{name}', '{http}')''')
+    con.commit()
+
+
+def del_category(name):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    if not is_category(name):
+        return False
+    con.cursor().execute(f'''DELETE from category WHERE name = "{name}"''')
+    con.commit()
+    return True
+
+
+def is_category(name):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    return len(con.cursor().execute(f'''SELECT * FROM category WHERE name="{name}"''').fetchall()) != 0
+
+
+def is_status(id_tg):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    return len(con.cursor().execute(f'''SELECT * FROM users WHERE id_tg="{id_tg}" and status = True''').fetchall()) != 0
+
+
+def is_assort(name):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    return len(con.cursor().execute(f'''SELECT * FROM assortment WHERE name="{name}"''').fetchall()) != 0
+
+
+def add_assort(name, opisanie, http, category):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''INSERT INTO assortment(name, http, description, category)
+                                 VALUES('{name}', '{http}', '{opisanie}', {category})''')
+    con.commit()
+
+
+def del_assort(name):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    if not is_category(name):
+        return False
+    con.cursor().execute(f'''DELETE from assortment WHERE name = "{name}"''')
+    con.commit()
+    return True
+
+
+def del_que_ans(question):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''DELETE from question_answer WHERE key_words = "{question}"''')
+    con.commit()
+
+
+def remove_answer(question, answer):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''UPDATE question_answer
+            SET answer = '{answer}' WHERE key_words = "{question}"''')
+    con.commit()
+
+
+def add_user(id_tg, name, username):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''INSERT INTO users(name, status, id_tg, username)
+                                 VALUES('{name}', False, {id_tg}, "{username}")''')
+    con.commit()
+
+
+def remove_status(id_tg):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''UPDATE users
+                    SET status = True WHERE id_tg = "{id_tg}"''')
+    con.commit()
+
+
+def get_discount():
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    return con.cursor().execute('''SELECT * FROM discounts''').fetchall()
+
+
+def add_discount(name, des):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''INSERT INTO discounts(name, des_if)
+                                         VALUES('{name}', "{des}")''')
+    con.commit()
+
+
+def remove_discount(name, des):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''UPDATE discounts
+                            SET des_if="{des}" WHERE name = "{name}"''')
+    con.commit()
+
+
+def del_discount(name):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''DELETE from discounts WHERE name = "{name}"''')
+    con.commit()
+
+
+def get_notification():
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    return con.cursor().execute('''SELECT * FROM notifications''').fetchall()
+
+
+def add_notification(text, time):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''INSERT INTO notifications(text, time)
+                                         VALUES("{text}", "{time}")''')
+    con.commit()
+
+
+def remove_notification(text, time):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''UPDATE notifications
+                            SET  time ="{time}" WHERE text="{text}"''')
+    con.commit()
+
+
+def del_notification(text):
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''DELETE from notifications WHERE  text = "{text}"''')
+    con.commit()
+
+
+def get_info_for_base():
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    itog = []
+    users = 'Пользователи', [
+        ('ID', 'ФИО', 'Должность(1-админ, 0-клиент', 'ID TG', 'UserName')] + con.cursor().execute(f'''SELECT id, name, 
+            status, id_tg, username FROM Users''').fetchall()
+    itog.append(users)
+    categories = 'Категории', [
+        ('ID Категории', 'Название категории', 'Путь к файлу картинки')] + \
+                 con.cursor().execute(f'''SELECT id, name, http FROM category''').fetchall()
+    itog.append(categories)
+    mailings = 'Уведомления', [
+        ('Сообщение', 'Дата отправления')] + \
+               con.cursor().execute(f'''SELECT text, time FROM notifications''').fetchall()
+    itog.append(mailings)
+    questions = 'Вопросы', [
+        ('Вопрос', 'Ответ')] + \
+                con.cursor().execute(f'''SELECT key_words, answer FROM question_answer''').fetchall()
+    itog.append(questions)
+
+    questions = 'Скидки', [
+        ('Название', 'Описание')] + \
+                con.cursor().execute(f'''SELECT name, des_if FROM discounts''').fetchall()
+    itog.append(questions)
+    workbook = xlsxwriter.Workbook('Таблица_Excel_БД.xlsx')
+    for sheet in itog:
+        name, stroki = sheet
+        worksheet = workbook.add_worksheet(name)
+        for row, stroka in enumerate(stroki):
+            for i in range(len(stroka)):
+                worksheet.write(row, i, stroka[i])
+    workbook.close()
+    return
+
+
+def dow_remove_for_tg(format):
+    if format:
+        del_all()
+    df = pd.read_excel(io='dow.xlsx', sheet_name=0)
+    book = df.head(10000).values
+    for lis in book:
+        add_user(int(lis[3]), lis[1], lis[4])
+        if lis[2]:
+            remove_status(int(lis[3]))
+    df = pd.read_excel(io='dow.xlsx', sheet_name=1)
+    book = df.head(10000).values
+    for lis in book:
+        add_category(lis[1], lis[2])
+    df = pd.read_excel(io='dow.xlsx', sheet_name=2)
+    book = df.head(10000).values
+    for lis in book:
+        add_notification(lis[0], lis[1])
+    book = df.head(10000).values
+    for lis in book:
+        add_que_ans(lis[0], lis[1])
+    book = df.head(10000).values
+    for lis in book:
+        add_discount(lis[0], lis[1])
+    print(book)
+
+
+def del_all():
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    con.cursor().execute(f'''DELETE from notifications''')
+    con.cursor().execute(f'''DELETE from assortment''')
+    con.cursor().execute(f'''DELETE from category''')
+    con.cursor().execute(f'''DELETE from discounts''')
+    con.cursor().execute(f'''DELETE from notifications''')
+    con.cursor().execute(f'''DELETE from question_answer''')
+    con.cursor().execute(f'''DELETE from users''')
+    con.commit()
+
+
+def get_no_admin_id():
+    con = sqlite3.connect('database.db', check_same_thread=False)
+    return con.cursor().execute(f'''SELECT id_tg FROM users WHERE status = False''')
+
+
+
+# add_que_ans('12', '34')
+# print(con.get_assort())
+# print(con.get_category_assort(0))
+# add_category('name', 'ooo')
+# del_category('Мужское')
+# print(con.is_category('12'))
+# print(con.is_assort('12'))
+# con.del_assort('12')
+# con.del_que_ans('12')
+# con.remove_answer('12', '78')
+# con.add_user('1233')
+# con.remove_status('12')
+# add_discount('12', '23')
+# print(con.get_discount())
+# con.remove_discount('12', '56')
+# con.del_discount('12')
+# add_notification('12', '122')
+# add_assort('789', '98', '00', 1)
+# print(con.get_notification())
+# con.remove_notification('12', '98')
+# con.del_notification('12')
+# print(get_info_for_base())
+# print(is_status(89))
+# dow_remove_for_tg(0)
+# add_category('Продукция с Aloe Vera', '1')
---
+++ main.py
@@ -0,0 +1,324 @@
+import datetime
+import logging
+import threading
+
+import schedule as schedule
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, ConversationHandler
+from for_db import *
+from geocod import *
+from work_of_api import *
+
+# Enable logging
+logging.basicConfig(filename='logging.log',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+                    )
+
+logger = logging.getLogger(__name__)
+
+TOKEN = "5342995443:AAEBqyRLrd5AmHEEhCNLyfHVy3td3Qvw-Ec"
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    add_user(user.id, f"{user.first_name} {user.last_name}", user.username)
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+    help_command(update, context)
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Отправит список команд, когда будет выдана команда /help."""
+    if is_status(update.message.chat_id):
+
+        await update.message.reply_text('Команды: \n/catalog - показывает каталог товаров магазина\n/document\n'
+                                        '/doc_post\n'
+                                        '/send_message\n'
+                                        '/stop\n')
+    else:
+        await update.message.reply_text('Команды: \n'
+                                        '/catalog - показывает каталог товаров магазина\n'
+                                        '/club_of_privileges\n'
+                                        '/joining_the_club\n'
+                                        '/geo\n'
+                                        '/administrator\n'
+                                        '/contacts\n'
+                                        '/statys\n'
+                                        '/stop\n')
+
+
+async def document_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Отправит xls файл, когда будет выдана команда /document."""
+    if is_status(update.effective_user.id):
+        get_info_for_base()
+        await update.message.reply_document(document='Таблица_Excel_БД.xlsx')
+    else:
+        await update.message.reply_text('У вас нет прав для данной команды.')
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Повторите сообщение пользователя."""
+    print(update.message.chat_id)
+    await update.message.reply_text(update.message.text)
+
+
+async def doc(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Ждет файл xlxs т администратора"""
+    await update.message.reply_text('Отпавте файл xlsx с изминениями')
+    return 0
+
+
+async def statys(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Назначает пользователя администратором, когда будет выдана команда /statys [password]."""
+    password = update.message.text[8:]
+    user = update.effective_user
+    if password == '1234':
+        remove_status(user.id)
+        await update.message.reply_html(rf"{user.mention_html()} назначен администратором!",
+                                        reply_markup=ForceReply(selective=True), )
+    else:
+        await update.message.reply_text('У вас нет прав!!!')
+
+
+async def check_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Повторите сообщение пользователя."""
+    a = update.message.document
+    if not a:
+        await update.message.reply_text('не то')
+        return ConversationHandler.END
+
+    get_file_of_tg(a.file_id, TOKEN)
+    if not check_file_of_tg():
+        await update.message.reply_text('pppp')
+    else:
+        await update.message.reply_text('Полностью ')
+        return 1
+    return ConversationHandler.END
+
+
+async def remove_bzd(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    dow_remove_for_tg(update.message.text)
+    await update.message.reply_text('несены изменения')
+
+
+async def catalog_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Отправит список разделов товаров, когда будет выдана команда /catalog."""
+    a = '\n'.join(list(map(lambda x: ' ' + str(x[0]) + ' - ' + x[1], get_category())))
+    await update.message.reply_text('Католог товаров у нас большой:\n' + a)
+    return 0
+
+
+async def contacts_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Отправит вырианты связи с магазином, когда будет выдана команда /contacts."""
+    await update.message.reply_text('У нас есть сайт, на котором можно найти много полезной информации. \n'
+                                    'Также присоединяйтесь к нам ВКонтакте.\n'
+                                    'Вконтакте: https://vk.com/soblaznarzamas \n'
+                                    'Сайт: https://soblaznarz.uds.app/c/join?ref=xvvs0921 \n')
+
+
+async def admin_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Связь с админимтратором, когда будет выдана команда /admin."""
+    await update.message.reply_text('Администратор ответит на все интересующие вас вопросы. '
+                                    'С ним можно связаться по телефону: +79202980333')
+
+
+async def geo_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Магазины на карте, когда будет выдана команда /geo."""
+    try:
+        maps = maps_global()
+        print(maps)
+        await update.message.reply_photo(maps)
+        await update.message.reply_text('У нас две точки по адресам:'
+                                        '\n\t 1. г.Арзамас, просп. Ленина, 121, TЦ «Метро» 3 здание, 1 этаж'
+                                        '\n\t 2. г.Арзамас, Парковая ул., 14А, ТЦ «Славянский»,1 этаж, отдел номер 7')
+    except RuntimeError as ex:
+        await update.message.reply_text('Что то пошло не по плану')
+
+
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Магазины на карте, когда будет выдана команда /geo."""
+    await update.message.reply_text('stop')
+    return ConversationHandler.END
+
+
+async def asortiment(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Магазины на карте, когда будет выдана команда /geo."""
+    text = update.message.text.rstrip()
+    # number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    # name = ['Наборы', 'Детская косметика', 'Лаки, пенки для волос, расчёски', ' Уход за волосами в домашних условиях',
+    #         'Косметика Мирра Люкс', 'Insight профуход за волосами', 'Крема для лица, тела и рук, очищение',
+    #         'Женские духи', 'Парфюм Niche- духи унисекс', 'Elements- парфюм унисекс', 'Продукция с Aloe Vera']
+    number = list(map(lambda x: str(x[0]), get_category()))
+    name = list(map(lambda x: x[1], get_category()))
+    if text in name:
+        up_text = get_assort_name_category(text)
+        if len(up_text) == 0:
+            await update.message.reply_text(f'В этой категории не присутствуют товары.')
+        else:
+            await update.message.reply_text(f'В этой категории присутствуют товары: \n')
+            for i in [[str(i[1]) + ' : ' + str(i[2]), i[3]] for i in up_text]:
+                await update.message.reply_photo(i[1])
+                await update.message.reply_text(i[0] + '\n')
+    elif text in number:
+        up_text = get_category_assort(int(text))
+        if len(up_text) == 0:
+            await update.message.reply_text(f'В этой категории не присутствуют товары.')
+        else:
+            await update.message.reply_text(f'В этой категории присутствуют товары: \n')
+            for i in [[str(i[1]) + ' : ' + str(i[2]), i[3]] for i in up_text]:
+                await update.message.reply_photo(i[1])
+                await update.message.reply_text(i[0] + '\n')
+    else:
+        print(text, number)
+        await update.message.reply_text(f'Возможно вы ошиблись.\nПопробйте ещё раз.')
+    return ConversationHandler.END
+
+
+async def joining_the_club_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """вступления в Клуб Привилегий, когда будет выдана команда /joining_the_club."""
+    await update.message.reply_text('Хочешь получать крутые преимущества? \n'
+                                    'Вступай в Клуб Привилегий. Что бы вступить нужно сделать некоторые действия: \n'
+                                    ' - Зайти в PlayMarket или AppStore и скачать приложение "UDS APP" \n'
+                                    ' - Зарегистрироваться и перейти по ссылке: '
+                                    'https://soblaznarz.uds.app/c/join?ref=dwac1210 \n'
+                                    ' - Получите первые 100 баллов на бонусный счет! (1 балл = 1 рубль) \n'
+                                    'Для того что бы ознокомиться с примуществами команда: /club_of_privileges')
+
+
+async def club_of_privileges_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Преимущества Клуба Привилегий, когда будет выдана команда /club_of_privileges."""
+    await update.message.reply_text('У нас есть Клуба Привилегий, участникам он даёт некоторые примущества:\n'
+                                    ' - Получают скидку на первую покупку \n'
+                                    ' - Получают персональные уведомления о новинках и акциях \n '
+                                    ' - Копят баллы с каждой покупки и забирают товар БЕСПЛАТНО \n'
+                                    ' - Рекомендуют нас друзьям и получают баллы с их покупок \n'
+                                    'Что бы ознокомится с инструкцией вступления команда: /joining_the_club')
+
+
+async def work_schedule_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Отправит график работы магазинов, когда будет выдана команда /catalog."""
+    await update.message.reply_text('У нас два магазина: \n'
+                                    ' - Магазин по адресу г.Арзамас, Парковая ул., 14А, ТЦ «Славянский». \n'
+                                    'Работает по графику: \n\t пн – пт 9.00-19.00 \n\t сб – вс 9.00-18.00 \n'
+                                    ' - Магазин по адресу г.Арзамас, просп. Ленина, 121, TЦ «Метро». \n'
+                                    'Работает по графику: \n\t пн – вс 9.00-20.00 \n'
+                                    'Будем вас в наших магазинах, '
+                                    'их место положение можно узнать с помощью команды /geo')
+
+
+async def send_of_admin_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Связь с админимтратором, когда будет выдана команда /admin."""
+    await update.message.reply_text('Введите текст, который вы планнируете отправить пользователям.')
+    return 0
+
+
+async def get_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Связь с админимтратором, когда будет выдана команда /admin."""
+    context.user_data['0'] = update.message.text
+
+    await update.message.reply_text('Введите дату в формате год:месяц:день, например, 2023:03:19\n'
+                                    'Если вы хотите отправить сообщение сейчас отправьте "сейчас".')
+    return 1
+
+
+def send_message(flag, text=''):
+    if flag:
+        today = ':'.join(
+            [str(datetime.date.today().year), str(datetime.date.today().month), str(datetime.date.today().day)])
+        print(today)
+        text = [i[1] for i in get_notification() if i[2] == today]
+        print(text)
+    for i in get_no_admin_id():
+        sendMessage(i, '\n'.join(text), TOKEN)
+
+
+async def get_time(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Связь с админимтратором, когда будет выдана команда /admin."""
+    if update.message.text == 'Сейчас':
+        send_message(True, text=context.user_data['0'])
+    print(context.user_data['0'])
+
+    await update.message.reply_text('')
+    return ConversationHandler.END
+
+
+def threat():  # второй поток для рассылки
+    while True:
+        schedule.run_pending()
+
+
+def main() -> None:
+    """Запустите бота."""
+    # Создайте приложение и передайте ему токен вашего бота.
+    application = Application.builder().token(TOKEN).build()
+    schedule.every().day.at("16:04").do(send_message, True)  # рассылка уведомлений
+    threading.Thread(target=threat).start()
+    script_doc = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('doc_p'
+                                     'ost', doc)],
+        # Состояние внутри диалога.
+        states={
+            0: [MessageHandler(filters.ALL & ~filters.COMMAND, check_file)],
+            1: [MessageHandler(filters.ALL & ~filters.COMMAND, remove_bzd)]
+        },
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        allow_reentry=False,
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    script_catalog = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('catalog', catalog_command)],
+        # Состояние внутри диалога.
+        states={
+            0: [MessageHandler(filters.TEXT & ~filters.COMMAND, asortiment)]
+        },
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        allow_reentry=False,
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    script_send = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler("send_message", send_of_admin_message)],
+        # Состояние внутри диалога.
+        states={
+            0: [MessageHandler(filters.ALL & ~filters.COMMAND, get_text)],
+            1: [MessageHandler(filters.ALL & ~filters.COMMAND, get_time)]
+        },
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        allow_reentry=False,
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    # по разным командам - отвечайте в Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("statys", statys))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("contacts", contacts_command))
+    application.add_handler(CommandHandler("administrator", admin_command))
+    application.add_handler(CommandHandler("geo", geo_command))
+    application.add_handler(CommandHandler("joining_the_club", joining_the_club_command))
+    application.add_handler(CommandHandler("club_of_privileges", club_of_privileges_command))
+    application.add_handler(script_doc)
+    application.add_handler(script_catalog)
+    application.add_handler(script_send)
+    application.add_handler(CommandHandler("document", document_command))
+    application.add_handler(CommandHandler("work_schedule", work_schedule_command))
+    # по некомандному, то есть сообщению - повторить сообщение в Telegram
+    createBD()
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Запускайте бота до тех пор, пока пользователь не нажмет Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
